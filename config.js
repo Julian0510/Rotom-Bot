89c4f2ae69d7c20f8a8555a9b1e1ce5f883b6568
Julian <juldiaz@csumb.edu>
2022-01-16 14:21:09 -0800

    First commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  consumer_key:         '',        //API keys, need to get your own if you want this to run
+  consumer_secret:      '',
+  access_token:         '',       //Access token keys, need to get your own if you want this to run
+  access_token_secret:  ''
+}
+
+module.exports = config;
